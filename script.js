490814733473d5fe5b6b44ea8dcdd28d56310011
Timi-G <56408619+Timi-G@users.noreply.github.com>
2023-09-25 12:09:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-async function load(){
+async function load() {
   // First, fetch events from the Google Apps Script API
-  const response= await fetch ('https://script.google.com/macros/s/AKfycbwZQnZiyq_zv3bmqjKETYdCmXo1-UXHcJAA1UmFvc5jrBhP4E7ybk4ra9Vs5toL4B9qEw/exec');
-  
-  const events= await response.json();
+  const response = await fetch('https://script.google.com/macros/s/AKfycbwZQnZiyq_zv3bmqjKETYdCmXo1-UXHcJAA1UmFvc5jrBhP4E7ybk4ra9Vs5toL4B9qEw/exec');
+
+  const events = await response.json();
 
   const eventsContainer = document.getElementById('events-container');
-  eventsContainer.innerHTML='';
-  
-  Loop through events and place them on page
+  eventsContainer.innerHTML = '';
+
+  // Loop through events and place them on page
   for (let event of events) {
-  // if column name has space, you can use
-  // event['Image URL']
+    // if column name has space, you can use
+    // event['Image URL']
     eventsContainer.innerHTML += `
       <div class="card w-72 bg-base-100 shadow-xl">
         <figure><img src="${event.Image}" alt="${event.Name}" /></figure>
@@ -25,20 +25,18 @@ async function load(){
     `
   }
 
-  // alternative way
-//   eventsContainer.innerHTML =  events.map((event) => `
-//       <div class="card w-72 bg-base-100 shadow-xl">
-//         <figure><img src="${event.Image}" alt="${event.Name}" /></figure>
-//         <div class="card-body">
-//           <h2 class="card-title">${event.Name}</h2>
-//           <p>${event.Location}<br/>@ ${event.Date}</p>
-//           <div class="card-actions justify-end">
-//             <button class="btn btn-primary" href="${event.Link}" target="_blank">RSVP</button>
-//           </div>
-//         </div>
-//       </div>
-//     `).join()
-// }
+  //   eventsContainer.innerHTML =  events.map((event) => `
+  //       <div class="card w-72 bg-base-100 shadow-xl">
+  //         <figure><img src="${event.Image}" alt="${event.Name}" /></figure>
+  //         <div class="card-body">
+  //           <h2 class="card-title">${event.Name}</h2>
+  //           <p>${event.Location}<br/>@ ${event.Date}</p>
+  //           <div class="card-actions justify-end">
+  //             <button class="btn btn-primary" href="${event.Link}" target="_blank">RSVP</button>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     `).join()
 
 }
 
